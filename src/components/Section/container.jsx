7d926592d04f8a1a9b06a3d13e0e0049875e7d49
Hanje Son <sonhanje@kakao.com>
2025-03-23 00:38:19 +0900

    add_util

--- src/components/Section/container.jsx
+++ src/components/Section/container.jsx
@@ -5,46 +5,53 @@ import { useTalkState } from '@/lib/TalkStateContext'
 import { useEffect, useState } from 'react'
 
 
+const debounce = (func, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
 export function Container () {
 
-    const [currentWindowWidth, setCurrentWindowWidth] = useState(null);
-    const { talkState, setTalkState } = useTalkState();
-  
-    const handleResize = () => {
-      setCurrentWindowWidth(window.innerWidth);
-    };
-  
-    useEffect(() => {
-    
-      if (typeof window !== 'undefined') {
-        handleResize();
-  
-        window.addEventListener('resize', handleResize);
-  
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }
-    }, []); 
-  
-    useEffect(() => {
+  const [currentWindowWidth, setCurrentWindowWidth] = useState(null);
+  const { talkState, setTalkState } = useTalkState();
 
-      if (currentWindowWidth !== null && currentWindowWidth < 600 && !talkState) {
-        setTalkState(true);
-      }
-    }, [currentWindowWidth, talkState, setTalkState]);
-  
-    if (currentWindowWidth === null) {
-      return null; 
+  const handleResize = debounce(() => {
+    setCurrentWindowWidth(window.innerWidth);
+  }, 300); 
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      handleResize(); 
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
     }
+  }, []);
 
+  useEffect(() => {
+    if (currentWindowWidth !== null && currentWindowWidth < 600 && !talkState) {
+      setTalkState(true);
+    }
+  }, [currentWindowWidth, talkState, setTalkState]);
 
-    return(
-        <div className={styles.container}>
-            { talkState &&
-                <TalkContainer className="author" />
-            }
-            <Content className="content" />
-        </div>
-    )
+  if (currentWindowWidth === null) {
+    return null; 
+  }
+
+  return (
+    <div className={styles.container}>
+      { talkState &&
+        <TalkContainer className="author" />
+      }
+      <Content className="content" />
+    </div>
+  );
 }
